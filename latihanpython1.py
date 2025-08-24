a109fd876f5018f9156778d3c9097d93dafed45c
Dk-Rud <2445oneros@gmail.com>
2025-08-24 15:45:44 +0700

    Uploud latihan pertama

---
+++ latihanpython1.py
@@ -0,0 +1,14 @@
+nama = "Diky Rudianto"
+umur = 19
+tinggi = 165.5
+hobi = ("belajar, belajar, belajar")
+mahasiswa = True
+print("Nama saya:", nama)
+print("Umur saya:", umur)
+print("Tinggi saya:", tinggi)
+print("Hobi saya:", hobi)
+print("Status mahasiswa:", mahasiswa)
+print(type(nama))
+print(type(umur))
+print(type(tinggi))
+print(type(mahasiswa))
